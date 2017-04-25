f5c42b6654f0c120f1130e7271605e52d5bc9599
Victor Parra <victor.parracant@gmail.com>
2017-04-25 23:38:08 +0200

    Class 2: Bounce a Heart Shaped Button

--- App.js
+++ App.js
@@ -1,88 +1,54 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry, Image, StyleSheet, Text, View, ScrollView, Dimensions, Animated
+  AppRegistry, Image, StyleSheet, Text, View, Animated, TouchableWithoutFeedback
 } from 'react-native';
 
-import Moment from "./src/Moment";
-
-const { width, height } = Dimensions.get("window");
-
-const Images = [
-  { image: require("./images/creaturebox1.jpg"), title: "Bot 1" },
-  { image: require("./images/creaturebox2.jpg"), title: "Bot 2" },
-  { image: require("./images/creaturebox1.jpg"), title: "Bot 3" },
-  { image: require("./images/creaturebox2.jpg"), title: "Bot 4" },
-];
-
-const getInterpolate = ( animatedScroll, i, imageLenth) => {
-  const inputRange = [
-    (i - 1) * width,
-    i * width,
-    (i + 1) * width,
-  ]
-  const outputRange = i === 0 ? [ 0, 0, 150 ] : [ -300, 0, 150 ];
-  return animatedScroll.interpolate({
-    inputRange,
-    outputRange,
-    extrapolate: "clamp"
-  })
-}
-
-const getSeparator = (i) => {
-  return
-    <View
-      key={i}
-      style={[styles.separate, {left: (i - 1) * width - 2.5}]}
-    />
-}
+import Heart from "./src/Heart";
 
 export default class RNanimations extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animatedScroll: new Animated.Value(0),
-      scrollEnabled: true
+      liked: false,
+      scale: new Animated.Value(0),
+      animations: [
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+      ]
     }
-    this.handleFocus = this.handleFocus.bind(this);
+    this.triggerLike = this.triggerLike.bind(this);
   }
-  handleFocus(focused) {
+  triggerLike() {
     this.setState({
-      scrollEnabled: !focused
+      liked: !this.state.liked
     })
+    Animated.spring(this.state.scale, {
+      toValue: 2,
+      friction: 3
+    }).start(() => {
+       this.state.scale.setValue(0);
+    });
   }
+
   render() {
+    const bouncyHeart = this.state.scale.interpolate({
+      inputRange: [0, 1, 2],
+      outputRange: [1, .8, 1]
+    })
+    const heartButtonStyle = {
+      transform: [ {scale: bouncyHeart} ]
+    }
     return (
       <View style={styles.container}>
-        <ScrollView
-          pagingEnabled
-          horizontal
-          scrollEnabled={this.state.scrollEnabled}
-          scrollEventThrottle={16}
-          onScroll={
-            Animated.event([
-              {
-                nativeEvent: {
-                  contentOffset: {
-                    x: this.state.animatedScroll
-                  }
-                }
-              }
-            ])
-          }
-        >
-          {Images.map((image, i) => {
-            return (
-              <Moment
-                key={i}
-                {...image}
-                translateX={getInterpolate(this.state.animatedScroll, i, Images.length)}
-                onFocus={this.handleFocus}
-                focused={!this.state.scrollEnabled}
-              />
-            )
-          })}
-          {Array.apply( null, { length: Images.length + 1}).map((_, i) => getSeparator(i))}
-        </ScrollView>
+        <TouchableWithoutFeedback onPress={this.triggerLike}>
+          <Animated.View style={heartButtonStyle}>
+            <Heart filled={this.state.liked}/>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
@@ -91,14 +57,9 @@ export default class RNanimations extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333',
-  },
-  separate: {
-    backgroundColor: "#000",
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    width: 5
+    backgroundColor: '#FFF',
+    justifyContent: "center",
+    alignItems: "center",
   }
 });
 
